d0a53dc4bd29c2676bb2f6ec1ae2dbfc16f39cd6
Romain Francoise <rfrancoise@debian.org>
2016-02-06 19:05:48 +0100

    Import upstream snapshot from commit 7669cfb330

--- cmd-show-environment.c
+++ cmd-show-environment.c
@@ -1,7 +1,7 @@
 /* $OpenBSD$ */
 
 /*
- * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
+ * Copyright (c) 2009 Nicholas Marriott <nicholas.marriott@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -34,11 +34,16 @@ void	 cmd_show_environment_print(struct cmd *, struct cmd_q *,
 	     struct environ_entry *);
 
 const struct cmd_entry cmd_show_environment_entry = {
-	"show-environment", "showenv",
-	"gst:", 0, 1,
-	"[-gs] " CMD_TARGET_SESSION_USAGE " [name]",
-	0,
-	cmd_show_environment_exec
+	.name = "show-environment",
+	.alias = "showenv",
+
+	.args = { "gst:", 0, 1 },
+	.usage = "[-gs] " CMD_TARGET_SESSION_USAGE " [name]",
+
+	.tflag = CMD_SESSION_CANFAIL,
+
+	.flags = 0,
+	.exec = cmd_show_environment_exec
 };
 
 char *
@@ -86,18 +91,13 @@ enum cmd_retval
 cmd_show_environment_exec(struct cmd *self, struct cmd_q *cmdq)
 {
 	struct args		*args = self->args;
-	struct session		*s;
 	struct environ		*env;
 	struct environ_entry	*envent;
 
-	if (args_has(self->args, 'g'))
-		env = &global_environ;
-	else {
-		s = cmd_find_session(cmdq, args_get(args, 't'), 0);
-		if (s == NULL)
-			return (CMD_RETURN_ERROR);
-		env = &s->environ;
-	}
+	if (args_has(self->args, 'g') || cmdq->state.tflag.s == NULL)
+		env = global_environ;
+	else
+		env = cmdq->state.tflag.s->environ;
 
 	if (args->argc != 0) {
 		envent = environ_find(env, args->argv[0]);
@@ -109,7 +109,10 @@ cmd_show_environment_exec(struct cmd *self, struct cmd_q *cmdq)
 		return (CMD_RETURN_NORMAL);
 	}
 
-	RB_FOREACH(envent, environ, env)
+	envent = environ_first(env);
+	while (envent != NULL) {
 		cmd_show_environment_print(self, cmdq, envent);
+		envent = environ_next(envent);
+	}
 	return (CMD_RETURN_NORMAL);
 }
